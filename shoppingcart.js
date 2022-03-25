f1c701f16299f28ba3296530f7f71a7bab5dcddf
Fevenf <101144416+Fevenf@users.noreply.github.com>
2022-03-25 08:42:27 -0400

    shopping cart

---
+++ shoppingcart.js
@@ -0,0 +1,36 @@
+//1
+let shopping = ['pop tarts', 'ramen noodles', 'chips', 'salsa', 'coffee']
+shopping.push('fruit loops') 
+console.log(shopping)
+shopping[4]='fair trade coffee'
+console.log(shopping)
+//4
+shopping[2,3]= 'rice', 'beans';
+console.log(shopping)
+//5
+let cart =[]
+//6
+shopping.pop()
+console.log(shopping)
+cart.push('fruit loops');
+console.log(cart);
+//7
+shopping.shift(0)
+console.log(shopping)
+cart.unshift('pop starts');
+console.log(cart);
+//8
+let i = 0
+while (shopping.length != 0) {
+    cart.push (shopping [i]);
+    shopping.shift();
+    i++;
+}
+//9
+console.log(cart);
+cart.sort()
+console.log(cart)
+cart.reverse()
+console.log(cart)
+//10
+console.log(cart.toString());
