9d00ca4bacc98b6dcc55191b6402d9b3c779bd2e
Fevenf <101144416+Fevenf@users.noreply.github.com>
2022-03-25 12:08:38 -0400

    array assignment

--- photo.js
+++ photo.js
@@ -1,30 +1,30 @@
-let photoslideshow = {
-    photolist: ["pic1", "pic2", "pic3"],
-    currentphotoindex: 0,
-    nextphoto: function () {
-      if (this.currentphotoindex < this.photolist.length - 1) {
-        return this.photolist[(this.currentphotoindex += 1)];
-      } else {
-        return (
-          this.photolist[(this.currentphotoindex = 0)],
-          console.log("End of slideshow.")
-        );
-      }
-    },
-    prevphoto: function () {
-      if (this.currentphotoindex <= 0) {
-        return this.photolist[
-          (this.currentphotoindex = this.photolist.length - 1)
-        ];
-      } else {
-        return (
-          this.photolist[(this.currentphotoindex -= 1)],
-          console.log("End of slideshow.")
-        );
-      }
-    },
-    getcurrentphoto: function () {
-      return this.photolist[this.currentphotoindex];
-    },
-  };
-  console.log(photoslideshow);
+// let photoslideshow = {
+//     photolist: ["pic1", "pic2", "pic3"],
+//     currentphotoindex: 0,
+//     nextphoto: function () {
+//       if (this.currentphotoindex < this.photolist.length - 1) {
+//         return this.photolist[(this.currentphotoindex += 1)];
+//       } else {
+//         return (
+//           this.photolist[(this.currentphotoindex = 0)],
+//           console.log("End of slideshow.")
+//         );
+//       }
+//     },
+//     prevphoto: function () {
+//       if (this.currentphotoindex <= 0) {
+//         return this.photolist[
+//           (this.currentphotoindex = this.photolist.length - 1)
+//         ];
+//       } else {
+//         return (
+//           this.photolist[(this.currentphotoindex -= 1)],
+//           console.log("End of slideshow.")
+//         );
+//       }
+//     },
+//     getcurrentphoto: function () {
+//       return this.photolist[this.currentphotoindex];
+//     },
+//   };
+//   console.log(photoslideshow);
